46f388c6ef0eb4064bf61ca4ddb89f657de89d04
Guilherme Tavares <guigo211103@gmail.com>
2023-02-28 17:52:53 -0300

    Configurado o outlet do header para aparecer em todas as rotas da aplicação / Corrigido erro na estrutura de pastas do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
-import { Router } from './components/Router'
+import { Router } from './Router'
 
 import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
