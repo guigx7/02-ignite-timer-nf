31184f29f4c300ef8830e9e11a811e3505f00a58
Guilherme Tavares <guigo211103@gmail.com>
2023-04-11 22:37:02 -0300

    Entendendo contextos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { Router } from './Router'
-
 import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
 
